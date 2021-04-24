10f0e00af9bf29de7efecde094f935534df8f599
Fish <conner.leuk@gmail.com>
2021-04-24 12:07:08 +0200

    quick bugfix patch

--- src/index.ts
+++ src/index.ts
@@ -3,21 +3,22 @@ import { ApplicationCommand, ApplicationCommandOptionType } from "./types";
 export { FishyClient };
 import { ApplicationCommandCompare } from "./utils/ApplicationCommandCompare";
 
-// Usefull for quick testing, only gets run when started with `node index.js`
+// Useful for quick testing, only gets run when started with `node index.js`
 if (require.main === module) {
   (async () => {
     require("dotenv").config();
-    let { GuildModel } = require("./test/models/Guild")
+    let { GuildModel } = require("./test/models/Guild");
     let client = new FishyClient({
       token: process.env.discord_token!,
       db_uri: process.env.mongodb_uri!,
       guild_model: GuildModel,
       author: "Fish#2455",
-      cmd_dir: "./lib/test/commands",
+      cmd_dir: "lib/test/commands",
       disable_load_on_construct: true,
+      dev_guild_id: "729791412459143278",
     });
     await client.load();
     await client.login();
-    console.log(client.categories)
+    console.log(client.categories);
   })();
 }
